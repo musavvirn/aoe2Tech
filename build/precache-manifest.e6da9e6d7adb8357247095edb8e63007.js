07ba1f47222126a7f99b154495cb91d2c0baebda
musavvir.noor <musavvir.noor@gmail.com>
2020-02-06 01:57:22 -0500

    updated build with more civilizations

---
+++ build/precache-manifest.e6da9e6d7adb8357247095edb8e63007.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a120e30c4041ab03e9aa4043230a031",
+    "url": "/aoe2Tech/index.html"
+  },
+  {
+    "revision": "79164b3164739923334a",
+    "url": "/aoe2Tech/static/css/2.5c2c0080.chunk.css"
+  },
+  {
+    "revision": "6ba8b9baf739fd7f3259",
+    "url": "/aoe2Tech/static/css/main.f6e80a94.chunk.css"
+  },
+  {
+    "revision": "79164b3164739923334a",
+    "url": "/aoe2Tech/static/js/2.0d113f74.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/aoe2Tech/static/js/2.0d113f74.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ba8b9baf739fd7f3259",
+    "url": "/aoe2Tech/static/js/main.3b0bafcd.chunk.js"
+  },
+  {
+    "revision": "0dab4363fb9ec6e3b8b0",
+    "url": "/aoe2Tech/static/js/runtime-main.771cfc87.js"
+  }
+]);
